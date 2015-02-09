7cf9d524eee1e7e9ab245b5fd1ba8adf83837450
Richard Fussenegger <richard@fussenegger.info>
2015-02-09 18:47:13 +0100

    Clean tasks are now directly in gulpfile.

--- gulp/tasks/clean.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-gulp.task('clean-dist', function gulpTaskCleanDist(done) {
-    $.del('dist', done);
-});
-
-gulp.task('clean-dev', function gulpTaskCleanDev(done) {
-    $.del('dev', done);
-});
